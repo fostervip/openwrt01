abb24eb4a161771e794a57cb75b24f90d0e28f46
fostervip <126537293+fostervip@users.noreply.github.com>
2023-03-20 08:43:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add a feed source
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
